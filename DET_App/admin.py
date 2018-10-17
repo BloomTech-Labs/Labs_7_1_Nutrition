a67449ee07a8bf806d67ef91cb88ca0b7ffd5827
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-10-17 17:02:58 -0500

    copy/paste job... running

--- DET_App/admin.py
+++ DET_App/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import (
+    Recipe, NutritionInfo, Ingredients
+)
 
 # Register your models here.
+
+class RecipeAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'last_modified')
+
+
+admin.site.register(
+    (Recipe, NutritionInfo, Ingredients),
+    RecipeAdmin
+)
