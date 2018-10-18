bce19cdfe1cb8202e3539b3d1aeba086098adf06
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-10-18 14:07:38 -0500

    test; rm db and contents

--- Dont_Eat_That/settings.py
+++ Dont_Eat_That/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
+import dj_database_url
+import os
+
 from decouple import config
 
-import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,6 +90,9 @@ DATABASES = {
     }
 }
 
+# for dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
