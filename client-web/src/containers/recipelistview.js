6b71ee05204dbe3b2a57e8f435386fde31f0fe14
shobanavv <shobanavv@gmail.com>
2018-10-24 10:54:59 -0700

    Initiating React webclient   for Nutrition app

---
+++ client-web/src/containers/recipelistview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Myrecipe from '../components/myrecipe.js';
+class RecipeList extends React.Component {
+    render() {
+        return (
+            <Myrecipe />
+        )
+    }
+}
+export default RecipeList;
