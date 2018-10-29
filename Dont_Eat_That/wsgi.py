957f0d2322819f4c1ab2eb85e1866bc8c32653d2
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-10-29 17:08:57 -0500

    heroku staticfiles error test

--- Dont_Eat_That/wsgi.py
+++ Dont_Eat_That/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dont_Eat_That.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate
 web: gunicorn Dont_Eat_That.wsgi --log-file -
