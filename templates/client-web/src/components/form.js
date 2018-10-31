8f2661f960d0d737315c367027307b1b2dd0596e
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-10-31 17:06:40 -0500

    original file from master

--- templates/client-web/src/components/form.js
+++ templates/client-web/src/components/form.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Form, Input, Button } from 'antd';
 import axios from 'axios';
@@ -5,72 +6,70 @@ import axios from 'axios';
 const FormItem = Form.Item;
 
 class CustomForm extends React.Component {
-	handleFormSubmit = (event, requestType, ingId) => {
-		event.preventDefault();
-		const cookTime = event.target.elements.CookTime.value;
-		const cookingMethod = event.target.elements.CookingMethod.value;
-		console.log(cookingMethod, cookTime); //data gets caught
-		switch (requestType) {
-			case 'post':
-				return axios
-					.post('http://127.0.0.1:8000/api/recipe/', {
-						CookTime: cookTime,
-						CookingMethod: cookingMethod
-					})
-					.then((res) => {
-						console.log('success');
-					})
-					.catch((err) => console.log('there was an error'));
+		handleFormSubmit = (event, requestType, ingId) => {
+			event.preventDefault();
+			const cookTime= event.target.elements.CookTime.value;
+			const cookingMethod = event.target.elements.CookingMethod.value;
+			console.log(cookingMethod,cookTime); //data gets caught
+			switch ( requestType ) {
+				case 'post':
+	return ( axios.post('http://127.0.0.1:8000/api/recipe/', {
 
-			case 'put':
-				return axios
-					.put(`http://127.0.0.1:8000/api/recipe/${ingId}/`, {
 						CookTime: cookTime,
 						CookingMethod: cookingMethod
 					})
-					.then((res) => console.log(res))
-					.catch((err) => console.log(err));
-		}
-	};
-	render() {
-		return (
-			<div>
-				<Form onSubmit={(event) => this.handleFormSubmit(event, this.props.requestType, this.props.ingId)}>
-					<FormItem label="Cook Time: ">
-						<Input name="CookTime" placeholder="Give a title to your recipe..." />
-					</FormItem>
-					<FormItem label="Cooking Method: ">
-						<Input name="CookingMethod" placeholder="Cooking instructions goes here..." />
-					</FormItem>
+					.then((res) => {console.log("success");})
+          .catch(err => console.log("there was an error"))
+          );
+		
+				case 'put':
+					return ( axios.put(`http://127.0.0.1:8000/api/recipe/${ingId}/`, {
+						CookTime: cookTime,
+						CookingMethod: cookingMethod
+						})
+						.then(res => console.log(res))
+            .catch(err => console.log(err))
+          );
+			}
+ 		}
+  render() {
+    return (
+      <div>
+        <Form onSubmit={(event) => this.handleFormSubmit(
+					event,
+					this.props.requestType,
+					this.props.ingId )}>
+          <FormItem label="Cook Time: ">
+            <Input name="CookTime" placeholder="Give a title to your recipe..." />
+          </FormItem>
+          <FormItem label="Cooking Method: ">
+            <Input name="CookingMethod" placeholder="Cooking instructions goes here..." />
+          </FormItem>
 					<FormItem label="Recipe Category: ">
-						<Input name="RecipeCategory" placeholder="Cooking instructions goes here..." />
-					</FormItem>
+            <Input name="RecipeCategory" placeholder="Cooking instructions goes here..." />
+          </FormItem>
 					<FormItem label="Recipe Cuisine: ">
-						<Input name="RecipeCuisine" placeholder="So, what cusine is this?..." />
-					</FormItem>
+            <Input name="RecipeCuisine" placeholder="So, what cusine is this?..." />
+          </FormItem>
 					<FormItem label="Recipe Instructions: ">
-						<Input name="RecipeInstructions" placeholder="Creative instructions goes here..." />
-					</FormItem>
+            <Input name="RecipeInstructions" placeholder="Creative instructions goes here..." />
+          </FormItem>
 					<FormItem label="Recipe Yield: ">
-						<Input name="RecipeYield" placeholder="how many in number..." />
-					</FormItem>
+            <Input name="RecipeYield" placeholder="how many in number..." />
+          </FormItem>
 					<FormItem label="Suitable For Diet: ">
-						<Input name="SuitableForDiet" placeholder="Suitable For what Diet..." />
-					</FormItem>
-					<FormItem label="Nutrition ">
-						<Input name="Nutrition" placeholder="Dropdown will show up" />
-					</FormItem>
-					<FormItem label="Recipe Ingredients: ">
-						<Input name="RecipeIngredients" placeholder="Ingredients goes here..." />
-					</FormItem>
-					<FormItem>
-						<Button type="primary" htmlType="submit">
-							{this.props.btnText}
-						</Button>
-					</FormItem>
-				</Form>
-			</div>
-		);
-	}
+            <Input name="SuitableForDiet" placeholder="Suitable For what Diet..." />
+          </FormItem><FormItem label="Nutrition ">
+            <Input name="Nutrition" placeholder="Dropdown will show up" />
+          </FormItem><FormItem label="Recipe Ingredients: ">
+            <Input name="RecipeIngredients" placeholder="Ingredients goes here..." />
+          </FormItem>
+          <FormItem>
+            <Button type="primary" htmlType="submit">{this.props.btnText}</Button>
+          </FormItem>
+        </Form>
+      </div>
+    );
+  }
 }
 export default CustomForm;
