13bbbec9cf1e25f370fdd9092554e65589b526c7
shobanavv <shobanavv@gmail.com>
2018-11-04 14:45:34 -0800

    Deploy Sandbox

--- Dont_Eat_That/settings.py
+++ Dont_Eat_That/settings.py
@@ -29,8 +29,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',
-                 'dont-eat-that.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['localhost', 
+                '127.0.0.1',
+                'det-sandbox.herokuapp.com', 
+                'dont-eat-that.herokuapp.com', 
+                '127.0.0.1:8000']
 
 
 # Application definition
