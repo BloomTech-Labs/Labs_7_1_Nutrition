954d4169adacc440ac95d8290c75016640d18d57
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-11-07 00:48:11 -0600

    env edit... failed attempt.

---
+++ DontEatThat/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lordorbnauticus/Documents/Dev/Lambda/Labs/Labs_7_1_Nutrition/DontEatThat/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
